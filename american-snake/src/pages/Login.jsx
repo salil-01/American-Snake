38462d5bad53c7606071bb9df675da03e73fcffc
Sourav Lahiri <cesouravlahiri4098@gmail.com>
2023-03-31 14:49:35 +0530

    login-signup

--- american-snake/src/pages/Login.jsx
+++ american-snake/src/pages/Login.jsx
@@ -3,11 +3,8 @@ import styled from "styled-components";
 import {
   Flex,
   Stack,
-  Link,
   Heading,
-  Text,
   useColorModeValue,
-  FormLabel,
 } from "@chakra-ui/react";
 import Footer from "../components/Homepage/Footer";
 
