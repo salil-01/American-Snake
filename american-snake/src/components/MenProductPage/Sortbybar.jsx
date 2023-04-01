601f42ee0d33e13677a8a6eee11aa3cbd1519e4c
Salil Upadhyay <upsalil.01@gmail.com>
2023-04-01 15:23:08 +0530

    added sort ui

--- american-snake/src/components/MenProductPage/Sortbybar.jsx
+++ american-snake/src/components/MenProductPage/Sortbybar.jsx
@@ -5,33 +5,33 @@ export const Sortbybar = ({ sortby }) => {
     console.log(e);
   };
   return (
-    <Box position="sticky" top={"20%"}>
-      <Flex>
-        <Box w={"60%"} bgGradient="linear(to-r, #fff5c8, #fcd5f5)" p={5}>
-          <Flex>
-            {sortby.map((e) => (
-              <Button colorScheme={"none"} color>
-                {e}
-              </Button>
-            ))}
-          </Flex>
+    <Box
+      position="sticky"
+      top={"20%"}
+      bgGradient="linear(to-r, #fff5c8, #fcd5f5)"
+      padding={"10px"}
+    >
+      <Box width={{ sm: "100%", md: "100%", lg: "90%" }} margin={"auto"}>
+        <Flex alignItems={"center"} justifyContent={"space-between"}>
           <Flex gap={4}>
             <Button
               fontSize={{ sm: "10px", md: "15px" }}
-              w={{ base: "20px", sm: "20px", md: "60px" }}
               colorScheme={"purple"}
             >
               All
             </Button>
             <Button
+              display={{ sm: "none", md: "none", lg: "inline" }}
               boxShadow={"sm"}
-              w={{ base: "100px", sm: "100px", md: "100px" }}
-              size={{ sm: "4xl", md: "4xl" }}
               fontSize={{ sm: "10px", md: "15px" }}
             >
               Try at Home
             </Button>
-            <Button boxShadow={"sm"} fontSize={{ sm: "10px", md: "15px" }}>
+            <Button
+              display={{ sm: "none", md: "none", lg: "inline" }}
+              boxShadow={"sm"}
+              fontSize={{ sm: "10px", md: "15px" }}
+            >
               Designs in Store
             </Button>
             <Button boxShadow={"sm"} fontSize={{ sm: "10px", md: "15px" }}>
@@ -41,23 +41,20 @@ export const Sortbybar = ({ sortby }) => {
               New In
             </Button>
           </Flex>
-        </Box>
-        <Flex w={"40%"} bgGradient="linear(to-r, #fcd5f5, #bfb3fe)">
-          <Spacer />
-          <Select
-            w={"40%"}
-            mt="30px"
-            bg
-            placeholder="Sort By:"
-            onClick={(e) => sorthandleclick(e)}
-          >
-            <option fontSize={{ sm: "10px" }} value="option1">
-              Price: Low to High
-            </option>
-            <option value="option2">Price: High to Low</option>
-          </Select>
+          <Box>
+            <Select
+              placeholder="Sort By : New Arrivals"
+              onClick={(e) => sorthandleclick(e)}
+              bg={"white"}
+            >
+              <option fontSize={{ sm: "10px" }} value="lowtohigh">
+                Price: Low to High
+              </option>
+              <option value="hightolow">Price: High to Low</option>
+            </Select>
+          </Box>
         </Flex>
-      </Flex>
+      </Box>
     </Box>
   );
 };
